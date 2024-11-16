79bf9cc7bf214db4a2a7294970ab964ea9869f65
Tiago Cardoso <tiagocardoso.dev@gmail.com>
2024-11-16 02:26:45 -0300

    add new test case
    say 'Hello, World' when the function receives an empty string

--- hello/hello.go
+++ hello/hello.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+const prefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello " + name
+	if name == "" {
+		name = "World"
+	}
+
+	return prefix + name
 }
 
 func main() {
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	result := Hello("Tiggas")
-	expected := "Hello Tiggas"
-
-	if result != expected {
-		t.Errorf("result '%s', expected '%s'", result, expected)
+	checkMessage := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("result '%s', expected '%s'", result, expected)
+		}
 	}
+
+	t.Run("say Hello to people", func(t *testing.T) {
+		result := Hello("Tiggas")
+		expected := "Hello, Tiggas"
+
+		checkMessage(t, result, expected)
+	})
+
+	t.Run("say 'Hello, World' when the function receives an empty string", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, World"
+
+		checkMessage(t, result, expected)
+	})
 }
